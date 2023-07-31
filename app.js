18edeb75e1e90d09c4bdf66db643821bbb99f301
ejsalvaran14 <141041026+ejsalvaran14@users.noreply.github.com>
2023-07-31 21:45:01 +0800

    first commit

---
+++ app.js
@@ -0,0 +1,40 @@
+// Landing effects
+const observer = new IntersectionObserver((entries)=>{
+    entries.forEach((entry) =>{
+        console.log(entry)
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show');
+        } else {
+            entry.target.classList.remove('show');
+        }
+    });
+});
+
+
+const hiddenElements = document.querySelectorAll('.hidden');
+hiddenElements.forEach((el) => observer.observe(el));
+
+// Project effects
+
+window.addEventListener('scroll', reveal);
+
+function reveal(){
+    var reveals = document.querySelectorAll('.reveal');
+
+    for(var i = 0; i < reveals.length; i++){
+        
+        var windowheight = window.innerHeight;
+        var revealtop = reveals[i].getBoundingClientRect().top;
+        var revealpoint = 150;
+
+        if(revealtop < windowheight - revealpoint){
+            reveals[i].classList.add('active');
+        }
+        else{
+            reveal[i].classList.remove('active');
+        }
+    }
+}
+
+
+
